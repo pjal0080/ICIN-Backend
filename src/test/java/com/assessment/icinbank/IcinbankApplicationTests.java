c4067d3c21a70027d43a5093dc783d6513d94ff0
pipranja <pipranja@cisco.com>
2021-11-16 22:16:06 +0530

    first commit

---
+++ src/test/java/com/assessment/icinbank/IcinbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.icinbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcinbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
