0ff8d2d8ddd9897d6fec42d3cd7882751911194a
pipranja <pipranja@cisco.com>
2021-12-02 21:46:30 +0530

    login test added successfully

--- src/test/java/com/assessment/icinbank/IcinbankApplicationTests.java
+++ src/test/java/com/assessment/icinbank/IcinbankApplicationTests.java
@@ -1,15 +1,16 @@
 package com.assessment.icinbank;
 
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.Set;
 
+@SpringBootTest
 class IcinbankApplicationTests {
 
 	@Test
@@ -20,7 +21,41 @@ class IcinbankApplicationTests {
 
 		WebDriver driver = new ChromeDriver();
 
+		String url = "http://localhost:4200/login";
+		driver.get(url);
+		System.out.println("Page is opened");
+		sleep(2000);
 
+
+		WebElement username = driver.findElement(By.xpath("//app-root/app-login//form//input[@type='text']"));
+		username.sendKeys("piyushpranjal27@gmail.com");
+		sleep(2000);
+
+		WebElement password = driver.findElement(By.xpath("//app-root/app-login//form//input[@type='password']"));
+		password.sendKeys("admin");
+		sleep(2000);
+
+		WebElement loginButton = driver.findElement(By.xpath("//app-root/app-login//form//button[@class='btn btn-primary']"));
+		loginButton.click();
+		sleep(2000);
+		System.out.println("Login successfull");
+
+		sleep(3000);
+		WebElement logoutButton = driver.findElement(By.xpath("//div[@id='navbarSupportedContent']/button[@type='submit']"));
+		logoutButton.click();
+		sleep(2000);
+		System.out.println("Logout successfull");
+
+		driver.quit();
+	}
+
+	public void sleep(long m) {
+
+		try {
+			Thread.sleep(m);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 	}
 
